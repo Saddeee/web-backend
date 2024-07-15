97fec788ca0039290db99aca5a8afbc1a084aec2
Saddeee <saddzyy@gmail.com>
2024-07-15 22:41:46 +0200

    chore connect to db

---
+++ modules/notes.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery',false)
+// const url = MONGODB_URL=`mongodb+srv://saddzyy:${password}@cluster0.mmpsump.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+console.log('connecting to db',)
+
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+
+noteSchema.set("toJSON",{
+  transform:(document, returnedObject)=>{
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
+module.exports = mongoose.model("Note",noteSchema)
